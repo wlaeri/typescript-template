6d338b86f12391d085ec6160f086fe67663b7fca
Will Laeri <wlaeri@gmail.com>
2021-12-28 22:39:35 -0500

    typedoc support

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+/**
+ * @module TypescriptTemplate
+ * @author Will Laeri <wlaeri@gmail.com> (https://github.com/wlaeri)
+ */
 export * from './sum';
--- src/sum.ts
+++ src/sum.ts
@@ -1,1 +1,7 @@
+/**
+ * @description A function that adds two numbers.
+ * @param a The first number to add.
+ * @param b The second number to add.
+ * @return {number} The sum of the two numbers.
+ */
 export const sum = (a: number, b: number): number => a + b;
